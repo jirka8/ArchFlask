025e72b1f7e57488487b67440b70670b07720c2f
jirka8 <jirka.dvorak@gmail.com>
2025-02-27 00:13:49 +0100

    - fix vyberu datumu nalezu - vyhozeni datepickeru a pouziti DateTimeLocalField

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
-from openpyxl.drawing.text import TextField
 from wtforms import Form, BooleanField, StringField, PasswordField, validators
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed, FileRequired
+import datetime
 from wtforms.fields.choices import SelectField, SelectMultipleField
 from wtforms.fields.datetime import DateTimeField, DateTimeLocalField
 from wtforms.fields.simple import TextAreaField, SubmitField, MultipleFileField
@@ -25,7 +25,7 @@ class CategoryForm(FlaskForm):
 class ItemsForm(FlaskForm):
     title = StringField('Nazev nálezu', validators=[validators.DataRequired()])
     description = TextAreaField('Popis nálezu', validators=[validators.DataRequired()])
-    found_at = DateTimeLocalField('Datum a čas nálezu', format='%m. %d. %Y')
+    found_at = DateTimeLocalField('Datum a čas nálezu', validators=[validators.DataRequired()], default=datetime.datetime.now())
     location = StringField('GPS souřadnice')
     area_id = SelectField('Lokalita', coerce=int, validate_choice=False)
     categories = SelectMultipleField('Kategorie', coerce=int, validators=[FileAllowed(['jpg', 'jpeg', 'png'], 'Pouze obrázky!')])
