988123d218b457a2e3749b71cdb4549dd16145bc
jirka8 <jirka.dvorak@gmail.com>
2025-02-26 21:25:59 +0100

    - env variables for db connection

--- database.py
+++ database.py
@@ -1,11 +1,18 @@
 import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
 
-#basedir = os.path.abspath(os.path.dirname(__file__))
+# db settings
+load_dotenv()
+db_user = os.getenv("DB_USER")
+db_pass = os.getenv("DB_PASS")
+db_host = os.getenv("DB_HOST")
+db_name = os.getenv("DB_NAME")
 
-#engine = create_engine('sqlite:///' + os.path.join(basedir, 'app.db'))
-engine = create_engine('postgresql://postgres:aspop@localhost/archeodb', echo=True)
+engine = create_engine(
+    'postgresql://%s:%s@%s/%s' % (db_user, db_pass, db_host, db_name),
+    echo=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
